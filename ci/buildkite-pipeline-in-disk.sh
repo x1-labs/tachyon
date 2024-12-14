59a1b865fb73cf6f11f9634d9dd951a70ae2b13a
Nicholas Pettas <nick@faircrypto.org>
2024-12-13 17:33:16 -0800

    Update CI name to Tachyon
    update the repo slug
    
    Update x1-agave-secondary pipeline name
    
    disable gcp uploads
    
    update the github actions

--- ci/buildkite-pipeline-in-disk.sh
+++ ci/buildkite-pipeline-in-disk.sh
@@ -114,8 +114,8 @@ EOF
 
 trigger_secondary_step() {
   cat  >> "$output_file" <<"EOF"
-  - name: "Trigger Build on solana-secondary"
-    trigger: "solana-secondary"
+  - name: "Trigger Build on tachyon-secondary"
+    trigger: "tachyon-secondary"
     branches: "!pull/*"
     async: true
     build:
@@ -289,7 +289,7 @@ if [[ -n $BUILDKITE_TAG ]]; then
   start_pipeline "Tag pipeline for $BUILDKITE_TAG"
 
   annotate --style info --context release-tag \
-    "https://github.com/anza-xyz/agave/releases/$BUILDKITE_TAG"
+    "https://github.com/FairCrypto/agave-xolana/releases/$BUILDKITE_TAG"
 
   # Jump directly to the secondary build to publish release artifacts quickly
   trigger_secondary_step
@@ -307,7 +307,7 @@ if [[ $BUILDKITE_BRANCH =~ ^pull ]]; then
 
   # Add helpful link back to the corresponding Github Pull Request
   annotate --style info --context pr-backlink \
-    "Github Pull Request: https://github.com/anza-xyz/agave/$BUILDKITE_BRANCH"
+    "Github Pull Request: https://github.com/FairCrypto/agave-xolana/$BUILDKITE_BRANCH"
 
   pull_or_push_steps
   exit 0
