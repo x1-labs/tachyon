199420bbdfb23201a2464a1e0b65a7d730ed844e
Nicholas Pettas <nick@faircrypto.org>
2024-12-13 17:13:13 -0800

    fix audit

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -47,6 +47,17 @@ cargo_audit_ignores=(
   # the server to exit cleanly on accepting a tcp/tls stream.
   # Ignoring because we do not use this functionality.
   --ignore RUSTSEC-2024-0376
+
+	# Crate:     idna
+	# Version:   0.1.5
+	# Title:     `idna` accepts Punycode labels that do not produce any non-ASCII when decoded
+	# Date:      2024-12-09
+	# ID:        RUSTSEC-2024-0421
+	# URL:       https://rustsec.org/advisories/RUSTSEC-2024-0421
+	# Solution:  Upgrade to >=1.0.0
+	# need to solve this depentant tree:
+	# jsonrpc-core-client v18.0.0 -> jsonrpc-client-transports v18.0.0 -> url v1.7.2 -> idna v0.1.5
+	--ignore RUSTSEC-2024-0421
 )
 scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
 # we want the `cargo audit` exit code, not `$dep_tree_filter`'s
