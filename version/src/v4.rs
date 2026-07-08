db341a5245dafc537aa8e7c1936501096af35890
Nicholas Pettas <nick@x1.xyz>
2026-07-08 16:43:14 -0700

    Add tachyon client and X1 binary aliases
    (cherry picked from commit f7962c5de8ad991718cf20097851ac9ca5f9df2d)

--- version/src/v4.rs
+++ version/src/v4.rs
@@ -690,7 +690,7 @@ mod tests {
             Version::new_from_parts(0, 0, 0, 0, 0, ClientId::this_client(), Prerelease::Stable);
         assert_eq!(
             version.as_detailed_string(),
-            "0.0.0 (src:00000000; feat:00000000, client:Agave)",
+            "0.0.0 (src:00000000; feat:00000000, client:Tachyon)",
         );
 
         let version = Version::new_from_parts(
@@ -704,7 +704,7 @@ mod tests {
         );
         assert_eq!(
             version.as_detailed_string(),
-            "0.0.0-rc.0 (src:00000000; feat:00000000, client:Agave)",
+            "0.0.0-rc.0 (src:00000000; feat:00000000, client:Tachyon)",
         );
     }
 }
